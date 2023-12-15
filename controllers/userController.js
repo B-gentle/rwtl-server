7ad54be2f9647f623d5ed36041230714ca2a56d6
beetech <brighteyo16@gmail.com>
2023-12-15 23:10:39 +0100

    ember promo removed

--- controllers/userController.js
+++ controllers/userController.js
@@ -853,7 +853,7 @@ const addDownline = asyncHandler(async (req, res) => {
     user.referralCode = generateReferralCode(stringId, user.username);
     user.referralLink = generateReferralLink(user.referralCode)
     user.walletBalance += selectedPackage.amount * selectedPackage.instantCashBack
-    user.emberPv += selectedPackage.pv
+    // user.emberPv += selectedPackage.pv
 
     //     //add upline
     user.upline = {
